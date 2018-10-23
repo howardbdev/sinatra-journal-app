6070d18a3cb25b631b6079d3c79ac4af6b17d327
howardbdev <howarddevennish@gmail.com>
2018-10-23 12:04:50 -0400

    migrate and seed db

---
+++ db/seeds.rb
@@ -0,0 +1,18 @@
+# here is where I will create some seed data to work with and test associations
+
+
+# Create 2 users
+
+howard = User.create(name: "Howard", email: "howard@howard.com", password: "password")
+sherry = User.create(name: "Sherry", email: "sherry@sherry.com", password: "password")
+
+# Create some journal entries
+
+JournalEntry.create(content: "Today is Tuesday and it's great!", user_id: howard.id)
+
+# Use AR to pre-associate data:
+
+howard.journal_entries.create(content: "We are building a Sinatra app!  Yaaay!")
+
+sherrys_entry = sherry.journal_entries.build(content: "It's super cold in VA!!!")
+sherrys_entry.save
