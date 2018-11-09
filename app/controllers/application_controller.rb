6c99bc842e407720244cb7773b8dc3922a3131e4
howardbdev <howarddevennish@gmail.com>
2018-11-08 19:24:27 -0500

    fixed weird error; added logic to root route

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,11 @@ class ApplicationController < Sinatra::Base
   end
 
   get "/" do
-    erb :welcome
+    if logged_in?
+      redirect "/users/#{current_user.id}"
+    else
+      erb :welcome
+    end
   end
 
   helpers do
