f49e264a9206ddb759c20d754dba2b4ebee5d065
howardbdev <howarddevennish@gmail.com>
2018-11-20 11:35:47 -0500

    add authorized_to_edit? helper method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -28,6 +28,10 @@ class ApplicationController < Sinatra::Base
       @current_user ||= User.find_by(id: session[:user_id])
     end
 
+    def authorized_to_edit?(journal_entry)
+      journal_entry.user == current_user
+    end 
+
   end
 
 end
