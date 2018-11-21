6b2f2e54aca1426a8cbf3dad8679fb926de2f7d4
howardbdev <howarddevennish@gmail.com>
2018-11-21 17:18:44 -0500

    register sinatra flash in app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,7 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
     enable :sessions
     set :session_secret, "our_awesome_journal_app"
+    register Sinatra::Flash
   end
 
   get "/" do
