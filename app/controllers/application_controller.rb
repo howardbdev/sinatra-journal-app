a6e1df85685b90c38147d63d75c5a1ef179b86a2
howardbdev <howarddevennish@gmail.com>
2018-11-27 12:58:40 -0500

    add helper methods around logged in status

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,11 +11,8 @@ class ApplicationController < Sinatra::Base
   end
 
   get "/" do
-    if logged_in?
-      redirect "/users/#{current_user.id}"
-    else
-      erb :welcome
-    end
+    redirect_if_logged_in
+    erb :welcome
   end
 
   helpers do
@@ -33,7 +30,20 @@ class ApplicationController < Sinatra::Base
       journal_entry.user == current_user
     end
 
-    # BULID HELPER METHOD FOR REDIRECTING IF NOT LOGGED IN!!
+    # use this helper method to protect controller actions where user must be logged in to proceed
+    def redirect_if_not_logged_in
+      if !logged_in?
+        flash[:errors] = "You must be logged in to view the page you tried to view."
+        redirect '/'
+      end
+    end
+
+    # use this helper method to avoid showing welcome, login, or signup page to a user that's already logged in
+    def redirect_if_logged_in
+      if logged_in?
+        redirect "/users/#{current_user.id}"
+      end
+    end
 
   end
 
