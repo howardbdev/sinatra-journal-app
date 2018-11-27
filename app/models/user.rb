79cf0fca641eeeb422cf2dd315dc19530e026649
howardbdev <howarddevennish@gmail.com>
2018-11-27 11:45:44 -0500

    add AR validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ActiveRecord::Base
   has_secure_password
+  validates :name, presence: true
+  validates :email, presence: true
+  validates :email, uniqueness: true
 
   has_many :journal_entries
 
