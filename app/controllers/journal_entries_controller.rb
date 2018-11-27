9c7963792d60d2818881082225ca986500e0908b
howardbdev <howarddevennish@gmail.com>
2018-11-27 12:59:01 -0500

    add flash messages and helper methods

--- app/controllers/journal_entries_controller.rb
+++ app/controllers/journal_entries_controller.rb
@@ -7,16 +7,15 @@ class JournalEntriesController < ApplicationController
 
   # get journal_entries/new to render a form to create new entry
   get '/journal_entries/new' do
+    redirect_if_not_logged_in
     erb :'/journal_entries/new'
   end
 
   # post journal_entries to create a new journal entry
   post '/journal_entries' do
+    redirect_if_not_logged_in
     # I want to create a new journal entry and save it to the DB
     # I also only want to create a journal entry if a user is logged in
-    if !logged_in?
-      redirect '/'
-    end
     # I only want to save the entry if it has some content
     if params[:content] != ""
       # create a new entry
@@ -24,7 +23,7 @@ class JournalEntriesController < ApplicationController
       @journal_entry = JournalEntry.create(content: params[:content], user_id: current_user.id)
       redirect "/journal_entries/#{@journal_entry.id}"
     else
-      flash[:message] = "Something went wrong."
+      flash[:errors] = "Something went wrong - you must provide content for your entry."
       redirect '/journal_entries/new'
     end
   end
@@ -43,33 +42,27 @@ class JournalEntriesController < ApplicationController
   # This route should send us to journal_entries/edit.erb, which will
   # render an edit form
   get '/journal_entries/:id/edit' do
+    redirect_if_not_logged_in
     set_journal_entry
-    if logged_in?
-      if authorized_to_edit?(@journal_entry)
-        erb :'/journal_entries/edit'
-      else
-        redirect "users/#{current_user.id}"
-      end
+    if authorized_to_edit?(@journal_entry)
+      erb :'/journal_entries/edit'
     else
-      redirect '/'
+      redirect "users/#{current_user.id}"
     end
   end
 
   # This action's job is to ...???
   patch '/journal_entries/:id' do
+    redirect_if_not_logged_in
     # 1. find the journal entry
     set_journal_entry
-    if logged_in?
-      if @journal_entry.user == current_user && params[:content] != ""
-      # 2. modify (update) the journal entry
-        @journal_entry.update(content: params[:content])
-        # 3. redirect to show page
-        redirect "/journal_entries/#{@journal_entry.id}"
-      else
-        redirect "users/#{current_user.id}"
-      end
+    if @journal_entry.user == current_user && params[:content] != ""
+    # 2. modify (update) the journal entry
+      @journal_entry.update(content: params[:content])
+      # 3. redirect to show page
+      redirect "/journal_entries/#{@journal_entry.id}"
     else
-      redirect '/'
+      redirect "users/#{current_user.id}"
     end
   end
 
@@ -77,6 +70,7 @@ class JournalEntriesController < ApplicationController
     set_journal_entry
     if authorized_to_edit?(@journal_entry)
       @journal_entry.destroy
+      flash[:message] = "Successfully deleted that entry."
       redirect '/journal_entries'
     else
       redirect '/journal_entries'
